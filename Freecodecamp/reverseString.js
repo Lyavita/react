ccf4a69fd09b8bb7d5cfcfa67f012e0f678b638a
Lyavita <lyavita@gmail.com>
2022-02-01 12:30:01 +0200

    task2

---
+++ Freecodecamp/reverseString.js
@@ -0,0 +1,24 @@
+// Reverse the provided string.
+// You may need to turn the string into an array before you can reverse it.
+// Your result must be a string. 
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+reverseString("hello");
+
+// ****** еще вариант
+
+// function reverseString(str) {
+//     let arr = [];
+//     let j;
+//     for (let i = str.length-1; i >= 0; i--) {    
+//         j = (i - str.length + 1) * (-1);
+//         arr[j] = str[i];   
+//     }
+    
+//     return arr.join(''); 
+// }
+
+// reverseString("hello");
